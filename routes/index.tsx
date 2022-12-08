868264015b0e096ad1d02339869da761052980d6
Steve Jeong <jst0930@gmail.com>
2022-12-08 10:14:35 +0300

    text-3xl

--- routes/index.tsx
+++ routes/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
         <title>Car Parking History</title>
       </Head>
       <div class="p-4 mx-auto max-w-screen-md border-solid border-4 border-indigo-600 rounded">
-        <p class="my-2 text-xl text-blue-600 font-bold">
+        <p class="my-2 text-3xl text-blue-600 font-bold">
           차량 주차 위치 이력
         </p>
         
